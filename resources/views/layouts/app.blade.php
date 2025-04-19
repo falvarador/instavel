1e8065a41b8cb6992dbd73542c673cca242411d0
Freddy Alvarado Rodríguez <falvarador@Freddys-MacBook-Pro.local>
2025-04-18 21:44:46 -0600

    feat: Add fortify and login view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+
+<body>
+    <div id="app">
+        <x-navbar />
+
+        <main class="container">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
